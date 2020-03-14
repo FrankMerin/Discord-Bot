5e1d53302404518de5e53616fff5f9e83f19fcd5
FrankMerin <61675381+FrankMerin@users.noreply.github.com>
2020-03-14 00:33:32 -0400

    temporary change while i work on database

--- cogs/uwufunction.py
+++ cogs/uwufunction.py
@@ -17,13 +17,16 @@ class uwufunction(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        uwu = 'uwu'
-        if(ctx.author.bot):
+        if ctx.guild.id != 482608530105434112:   # temporary fix while i work on database solution to prevent UwU from running a specific server
+            uwu = 'uwu'
+            if(ctx.author.bot):
+                return
+            if ctx.content.lower() == uwu:
+                channel = ctx.channel
+                await channel.send('UwU')
+                await self.client.process_commands(ctx)
+        else:
             return
-        if ctx.content.lower() == uwu:
-            channel = ctx.channel
-            await channel.send('UwU')
-            await self.client.process_commands(ctx)
 
 
         
